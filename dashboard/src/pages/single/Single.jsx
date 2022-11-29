d2e3ac1aa38b55c9356aae077774c10e151a2db6
Segun Osiki <osiki.segun@gmail.com>
2022-11-29 09:05:15 +0100

    added single page

--- dashboard/src/pages/single/Single.jsx
+++ dashboard/src/pages/single/Single.jsx
@@ -1,7 +1,16 @@
+import "./single.scss"
+import Sidebar from "../../components/sidebar/Sidebar"
+import NavBar from "../../components/navbar/NavBar"
+
 const Single = () => {
     return (
-        <div>
-            Single
+        <div className="single">
+            <Sidebar />
+
+            <div className="singleContainer">
+                <NavBar />
+                Single
+            </div>
         </div>
       );
 }
