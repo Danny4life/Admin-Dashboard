c11f1110181c7a1887787683e43678d3b2669523
Segun Osiki <osiki.segun@gmail.com>
2022-11-29 19:00:06 +0100

    added links to users and products page

--- dashboard/src/components/sidebar/Sidebar.jsx
+++ dashboard/src/components/sidebar/Sidebar.jsx
@@ -14,8 +14,8 @@ const Sidebar = () => {
     return (  
         <div className="sidebar">
             <div className="top">
-                <Link to="/">
-                    <span className="logo">
+                <Link to="/" style={{textDecoration: "none"}}>
+                    <span className="logo" >
                         SegunAdmin
                     </span>
                 </Link>
@@ -31,14 +31,18 @@ const Sidebar = () => {
                         <span>Dashboard</span>
                     </li>
                     <p className="title">LIST</p>
+                    <Link to="/users" style={{textDecoration: "none"}}>
                     <li>
                       <PersonIcon className="icon" />
                         <span>Users</span>
                     </li>
+                    </Link>
+                    <Link to="/products" style={{textDecoration: "none"}}>
                     <li>
                         <StoreIcon className="icon" />
                         <span>Products</span>
                     </li>
+                    </Link>
                     <li>
                         <CreditCardIcon className="icon" />
                         <span>Orders</span>
