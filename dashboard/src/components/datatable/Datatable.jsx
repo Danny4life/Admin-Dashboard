ff0d0488295ae378ed0bcabba1c618a412b2c468
Segun Osiki <osiki.segun@gmail.com>
2022-11-29 19:04:07 +0100

    added link to view button to users page

--- dashboard/src/components/datatable/Datatable.jsx
+++ dashboard/src/components/datatable/Datatable.jsx
@@ -2,6 +2,7 @@ import "./datatable.scss"
 
 import { DataGrid } from '@mui/x-data-grid';
 import { userColumns, userRows } from "../../datatablesource";
+import { Link } from "react-router-dom"
 
 
 
@@ -11,7 +12,10 @@ const Datatable = () => {
     const actionColumn = [{field: "action", headerName: "Action", width: 200, renderCell:()=>{
         return (
             <div className="cellAction">
+            <Link to="/users/test" style={{textDecoration: "none"}}>
                 <div className="viewButton">View</div>
+            </Link>
+             
                 <div className="deleteButton">Delete</div>
             </div>
         )
