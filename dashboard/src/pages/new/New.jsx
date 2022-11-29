46b19a5eebc7abf250a9409973e02e31863dfbe6
Segun Osiki <osiki.segun@gmail.com>
2022-11-29 16:28:26 +0100

    added form to user new page

--- dashboard/src/pages/new/New.jsx
+++ dashboard/src/pages/new/New.jsx
@@ -1,15 +1,44 @@
 import "./new.scss"
 import Sidebar from "../../components/sidebar/Sidebar"
 import NavBar from "../../components/navbar/NavBar"
+import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 
 
-const New = () => {
+const New = ({ inputs, title }) => {
     return ( 
         <div className="new">
             <Sidebar />
             <div className="newContainer">
                 <NavBar />
-                test
+                <div className="top">
+                    <h1>{title}</h1>
+                </div>
+                <div className="bottom">
+                    <div className="left">
+                        <img
+                        src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg" alt="" />
+                    </div>
+                    <div className="right">
+                    <form>
+                        <div className="formInput">
+                            <label htmlFor="file">
+                               Image: <DriveFolderUploadOutlinedIcon className="icon" />
+                            </label>
+                            <input type="file" id="file" style={{display: "none"}} />
+                        </div>
+
+                        {inputs.map((input)=> (
+
+                            <div className="formInput" key={input.id}>
+                            <label>{input.label}</label>
+                            <input type={input.type} placeholder={input.placeholder} />
+                        </div>
+
+                        ))}
+                        <button>Send</button>
+                    </form>
+                    </div>
+                </div>
             </div>
         </div>
      );
