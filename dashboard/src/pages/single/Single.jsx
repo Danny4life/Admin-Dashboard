b75b167acab05d7c9c3d2ecc6cd7e1f05f4e5bc3
Segun Osiki <osiki.segun@gmail.com>
2022-11-29 13:19:25 +0100

    added user information card details

--- dashboard/src/pages/single/Single.jsx
+++ dashboard/src/pages/single/Single.jsx
@@ -9,7 +9,37 @@ const Single = () => {
 
             <div className="singleContainer">
                 <NavBar />
-                Single
+                <div className="top">
+                    <div className="left">
+                        <div className="editButton">Edit</div>
+                        <h1 className="title">Information</h1>
+                        <div className="item">
+                            <img src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" className="itemImg" />
+                            <div className="details">
+                                <h1 className="itemTitle">Jane Doe</h1>
+                                <div className="detailItem">
+                                    <span className="itemKey">Email:</span>
+                                    <span className="itemValue">janedoe@gmail.com</span>
+                                </div>
+                                <div className="detailItem">
+                                    <span className="itemKey">Phone:</span>
+                                    <span className="itemValue">+1 2345 6789 00</span>
+                                </div>
+                                <div className="detailItem">
+                                    <span className="itemKey">Address:</span>
+                                    <span className="itemValue">Jane Doe st. Garfiend yard, New york.</span>
+                                </div>
+                                <div className="detailItem">
+                                    <span className="itemKey">Country:</span>
+                                    <span className="itemValue">USA</span>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div className="right"></div>
+                </div>
+                <div className="bottom"></div>
             </div>
         </div>
       );
