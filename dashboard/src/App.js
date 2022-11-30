b54d146ef373e834e485f63a1b6cbc0512324bb7
Segun Osiki <osiki.segun@gmail.com>
2022-11-30 07:28:22 +0100

    added admin-dashboard as home page path

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -26,7 +26,7 @@ function App() {
     <div className={darkMode ? "app dark" : "app"}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/admin-dashboard" element={<Home />} />
           <Route path="login" element={<Login />}/>
           <Route path="users">
             <Route index element={<List />} />
